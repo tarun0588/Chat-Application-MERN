b5ba7ffed1cb3c7883ba65c05ee10925a67d84fe
Tarun Sharma <tarunsharma0588@gmail.com>
2024-09-10 01:26:26 +0530

    env changes done

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/connectDB')
 const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
+const path = require ('path')
 
 // const app = express()
 app.use(cors({
@@ -16,12 +17,26 @@ app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
 
-app.get('/',(request,response)=>{
-    response.json({
-        message : "Server running at " + PORT
-    })
-})
 
+
+// *************Deployment section***************
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,'/client/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1,'client','build','index.html'))
+    })
+
+} else {
+    app.get('/',(request,response)=>{
+        response.json({
+            message : "Server running at " + PORT
+        })
+    })
+    
+}
+
+// *************Deployment section***************
 //api endpoints
 app.use('/api',router)
 
